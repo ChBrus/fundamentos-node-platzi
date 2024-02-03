ce53705eb0aacb4c2bc087f99c897f980b453e61
ChBrus <cbruno.carrillo@gmail.com>
2024-02-03 14:23:23 -0600

    Clase: Globals

---
+++ modulos/globales.js
@@ -0,0 +1,13 @@
+// let i = 1
+
+// let intervalo = setInterval(() => {
+//     if (i === 3) clearInterval(intervalo)
+
+//     console.log('hola')
+
+//     i++
+// }, 1000)
+
+global.miVariable = 'Valor'
+
+console.log(miVariable)
